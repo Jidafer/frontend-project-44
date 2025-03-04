e171f509ff52f75422484f424f3fb578b0219c51
Konstantin <valerijuprisiv1108@gmail.com>
2025-03-04 10:03:47 +0300

    solution

--- Makefile
+++ Makefile
@@ -1,11 +1,10 @@
-
 install: 
 	npm ci;
 	npm install readline-sync;
 	chmod +x bin/brain-games.js
 brain-games:
 	node bin/brain-games.js
-make lint: 
+ lint: 
 	npx eslint .
 publish: 
 	 npm publish --dry-run
--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,5 @@ import readlineSync from 'readline-sync';
 
 export default  () => {
 console.log ('node bin/brain-games.js\nWelcome to the Brain Games!');
-const name = readline-Sync.question('May I have your name?') ;
+const name = readlineSync.question('May I have your name?') ;
  console.log (`Hello, ${name}!`)} ;
